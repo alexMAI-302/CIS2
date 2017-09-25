9a9f8a1697a1a65cc2b23be74872b283eaa429c5
CS302 <luckycloud@yandex.ru>
2017-09-25 23:22:35 +0400

    pub2
    pub2

--- CIS2/Form1.cs
+++ CIS2/Form1.cs
@@ -96,29 +96,38 @@ namespace CIS2
         private void _btnGo_Click(object sender, EventArgs e)
         {
             Matrix mtx = new Matrix();
+            string mtxJson = string.Empty;
+
             for (int i = 0; i < _dGW.Rows.Count; i++)
             {
                 mtx.matrix.Add(new double[_dGW.Rows.Count]);
                 for (int j = 0; j < _dGW.Columns.Count; j++)
                     mtx.matrix[i][j] = Convert.ToDouble(_dGW[j, i].Value);
             }
-            DataContractJsonSerializer serializer = new DataContractJsonSerializer(typeof(Matrix));
-            using (FileStream stream = new FileStream("matrix.json", FileMode.Create))
+            
+            //JSON сохраняется в ОП, а затем считывается в строку
+            using (MemoryStream memory = new MemoryStream())
             {
-                serializer.WriteObject(stream, mtx);
+                DataContractJsonSerializer serializer = new DataContractJsonSerializer(typeof(Matrix));
+                serializer.WriteObject(memory, mtx);
+                using (StreamReader reader = new StreamReader(memory))
+                {
+                    memory.Position = 0;
+                    mtxJson = reader.ReadToEnd();
+                }
             }
 
             //POST
             string response = string.Empty;
             string URI = "http://www.ws-dss.com/ws_jobs.json";
-            string postparams = String.Format("ws_job[ws_method_id]=30&ws_job[input]={0}", File.ReadAllText("matrix.json"));
+            string postparams = String.Format("ws_job[ws_method_id]=30&ws_job[input]={0}", mtxJson);
             
             HttpWebRequest req = (HttpWebRequest)WebRequest.Create(URI);
             req.Method = "POST";
             req.ContentLength = postparams.Length;
             req.Headers.Add("user-token", _TBtoken.Text);
 
-            using (var writer = new StreamWriter(req.GetRequestStream()))
+            using (StreamWriter writer = new StreamWriter(req.GetRequestStream()))
             {
                 writer.Write(postparams);
             }
@@ -139,8 +148,18 @@ namespace CIS2
                 }
             }
 
-            string output = response.Substring(response.IndexOf("output"));
-            _rTB.Text = output;
+            Result result = new Result(mtx.matrix.Count);
+            using (MemoryStream memory = new MemoryStream(Encoding.UTF8.GetBytes(Result.GetJ(response))))
+            {
+                DataContractJsonSerializer serializer = new DataContractJsonSerializer(result.GetType());
+                result = (Result)serializer.ReadObject(memory);
+            }
+
+            string weights = string.Empty;
+            for (int i = 0; i < mtx.matrix.Count; i++)
+                weights += result.weight[i] + ",\n";
+            
+            _rTB.Text = String.Format("Weights:\n{0}\nl_max: {1}\nconsistency index: {2}", weights, result.l_max, result.consistency_index);
         }
     }
 }
---
+++ CIS2/Result.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CIS2
+{
+    [DataContract]
+    public class Result
+    {
+        [DataMember(Name = "weight")]
+        public double[] weight { get; set; }
+
+        [DataMember(Name = "l_max")]
+        public double l_max { get; set; }
+
+        [DataMember(Name = "cosistecy idex")]
+        public double consistency_index { get; set; }
+
+        [DataMember(Name = "radom cosistecy idex")]
+        public double random_consistency_index { get; set; }
+
+        [DataMember(Name = "cosistecy ratio")]
+        public double consistency_ratio { get; set; }
+
+        public static string GetJ(string js)
+        {
+            string json = string.Empty;
+            json = "{\"" + js.Substring(js.IndexOf("weight"));
+            json = json.Substring(0, json.IndexOf("\",\""));
+            json = json.Replace("\n", "");
+            json = json.Replace("\\", "");
+            json = json.Replace("n", "");
+            return json;
+        }
+
+        public Result(int n)
+        {
+            weight = new double[n];
+        }
+    }
+}
